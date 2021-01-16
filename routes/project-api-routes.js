269737ea5ad95cf0f0645a9ce922640890c0c944
Wagon Wheel <70062069+njleoni@users.noreply.github.com>
2021-01-16 14:15:59 -0700

    push all

---
+++ routes/project-api-routes.js
@@ -0,0 +1,32 @@
+// Requiring our models
+const db = require('../models');
+
+// Routes
+module.exports = (app) => {
+  // GET route for getting all of the todos
+  app.get('/all', (req, res) => {
+    // findAll returns all entries for a table when used with no options
+    
+    db.Project.findAll({})
+      .then((data) => {
+        const jsonValues = JSON.parse(JSON.stringify(data));
+        res.render('index', {
+          Projects: jsonValues,
+        });
+    });
+  });
+
+  app.post('/post', (req, res) => {
+    // findAll returns all entries for a table when used with no options
+    
+    // db.Project.findAll({})
+    //   .then((data) => {
+    //     const jsonValues = JSON.parse(JSON.stringify(data));
+    //     res.render('index', {
+    //       Projects: jsonValues,
+    //     });
+    // });
+  });  
+
+  // app.get('/', (req, res) => res.send('Project'));
+};
--- server.js
+++ server.js
@@ -1,17 +1,32 @@
+// - Dependency
 const express = require('express');
+const Handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 
+// - Sets up express app
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// - Requires our models for syncing
 const db = require('./models');
 
-app.use(express.urlencoded({ extended: true }));
+// - Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-app.use(express.static('public'));
+// - Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-require('./routes/api-routes')(app);
+// - Sets handlebars
+const exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main", handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+app.set("view engine", "handlebars");
 
-db.sequelize.sync().then(() => {
-    app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+// Routes
+require('./routes/project-api-routes.js')(app);
+
+// - Syncing our sequelize models and then starting our Express app
+db.sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 });
+
