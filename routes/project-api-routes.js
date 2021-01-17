59fd3ef24b5c4f96feb9c17580b799dfa00048c9
Wagon Wheel <70062069+njleoni@users.noreply.github.com>
2021-01-17 14:30:16 -0700

    update post route, did not get it to work

--- routes/project-api-routes.js
+++ routes/project-api-routes.js
@@ -3,6 +3,10 @@ const db = require('../models');
 
 // Routes
 module.exports = (app) => {
+
+  app.get('/', (req, res) => res.redirect('/all'));
+
+
   // GET route for getting all of the todos
   app.get('/all', (req, res) => {
     // findAll returns all entries for a table when used with no options
@@ -16,17 +20,61 @@ module.exports = (app) => {
     });
   });
 
+  app.get('/post', (req, res) => res.render('post'));
+
   app.post('/post', (req, res) => {
-    // findAll returns all entries for a table when used with no options
-    
-    // db.Project.findAll({})
-    //   .then((data) => {
-    //     const jsonValues = JSON.parse(JSON.stringify(data));
-    //     res.render('index', {
-    //       Projects: jsonValues,
-    //     });
-    // });
-  });  
+    console.log(req.body.project_name);
+      db.Project.create({
+        project_name: req.body.project_name,
+        project_status: req.body.project_status,
+        project_manager: req.body.project_managert,
+        percent_complete: req.body.percent_complete,
+        start_date: req.body.start_date,
+        due_date: req.body.due_date,
+      }).then((project) => 
+      res.json(project));
+      res.redirect('/all');
+  })
+
+  // app.post('/post', (req, res) => {
+  //   const { project_name, project_status, project_manager, percent_complete, start_date, due_date} = req.body;
+  //   const errors = [];
+  //   if(!project_name) {
+  //     errors.push({ text: "Please add a project name" });
+  //   }
+  //   if(!project_status) {
+  //     errors.push({ text: "Please add a project status" });
+  //   }
+  //   if(!project_name) {
+  //     errors.push({ text: "Please add a project manager" });
+  //   }
+  //   if(!project_name) {
+  //     errors.push({ text: "Please add a project due date" });
+  //   }        
+  //   if(errors.length > 0) {
+  //     res.render('post', {
+  //       errors,
+  //       project_name,
+  //       project_status,
+  //       project_manager,
+  //       percent_complete,
+  //       start_date,
+  //       due_date
+  //     });
+  //   }
+  //   else {
+  //     Projects.create({
+  //       project_name,
+  //       project_status,
+  //       project_manager,
+  //       percent_complete,
+  //       start_date,
+  //       due_date
+  //     })
+  //     .then(post => res.redirect('/all'))
+  //     .catch(err => console.log(err));
+  //   }
+  // });
 
   // app.get('/', (req, res) => res.send('Project'));
 };
